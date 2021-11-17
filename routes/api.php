1d3ad88850fc69d70c1f5924812afc06d66a908f
Raheel Khan <irahilkhan@gmail.com>
2021-11-17 18:10:06 +0500

    mood, media links, api resource media

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('moods', [\App\Http\Controllers\Api\MoodApiController::class, 'index']);
+Route::get('medias', [\App\Http\Controllers\Api\MoodApiController::class, 'index']);
