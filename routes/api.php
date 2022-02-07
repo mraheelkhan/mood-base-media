30efe633defd93a9291839896a67b1118284c5ab
Raheel Khan <irahilkhan@gmail.com>
2022-02-07 12:44:08 +0500

    flutter api configuration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    
+});
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+
 
 Route::get('medias', [\App\Http\Controllers\Api\MoodApiController::class, 'index']);
